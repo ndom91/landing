2635440e84fe50ffa71eae444ee124e5e8b17af2
ndo_nt <yo@iamnico.xyz>
2019-01-22 09:15:52 +0100

    major rewrite

--- assets/script.js
+++
@@ -1,4 +0,0 @@
-var path = 'wallpapers/',
-    imgs = ['google-earth-view-6579.jpg','google-earth-view-6311.jpg','google-earth-view-6260.jpg'],
-    i = Math.floor(Math.random()*imgs.length);
-	document.getElementById('.bg').style.backgroundImage = 'url("+path+imgs[i]+")'; 
